7c9fa3c8d939da1b04c2ce505e8b271886a57fa7
Shwetabh Sharan <shwetabhsharan@gmail.com>
2015-05-02 01:43:58 +0530

    Partial work commit

--- github-manager.py
+++ github-manager.py
@@ -1,22 +1,13 @@
 """
-Module to create csv for list of issues for a repository
-Usage: python3 github-manager.py
-After executing, you will see the following
-
-======github to csv exporter======
-Enter your github username: shwetabhs
-Password: 
-Enter org name: SS-priv
-Enter repository name: himalayan_pink
-
-Enter the above values and it will print a list of issues present
-
-TODO: create csv
 """
+
 import getpass
 import os
 import requests
 import sys
+import csv
+
+HEADER_LIST = ['ID', 'Title', 'State', 'Created At', 'Updated At', 'Closed At', 'Assignee', 'Label']
 
 class GithubManager(object):
 
@@ -43,7 +34,7 @@ class GithubManager(object):
             sys.exit(1)
 
         # get org info, returns a list of urls, extract org url
-        self.get_org_info()
+        self._get_org_info()
 
         # get repo info
         self.issue_url=self.get_repository_info()
@@ -51,7 +42,7 @@ class GithubManager(object):
         # retrive issue list
         self.retrive_issue_list()
 
-    def request_handler(self, url):
+    def _request_handler(self, url):
         """
         use request module to call apis
         """
@@ -62,9 +53,11 @@ class GithubManager(object):
 
     def _authenticate_user(self):
         """
+        authenticate user by accessing github username and password and
+        see if the response is 200. If not, exit with error message.
         """
 
-        response = self.request_handler(url='https://api.github.com')
+        response = self._request_handler(url='https://api.github.com')
 
         if response.status_code == 200:
             print("Authentication successful. Status: {0}".format(response.status_code))
@@ -74,33 +67,58 @@ class GithubManager(object):
             print("Unauthorized access. Status: {0}".format(response.status_code))
             return False
 
-    def get_org_info(self):
+    def _get_org_info(self):
         """
+        get api directory for the selected organization
         """
 
         url='https://api.github.com/orgs/{0}'.format(self.org_name)
-        repo_info = self.request_handler(url)
+        repo_info = self._request_handler(url)
         org_url_directory = repo_info.json()
         self.repos_url = org_url_directory.get('repos_url')
 
     def get_repository_info(self):
         """
+        get repo informationm
         """
 
-        repo_info = self.request_handler(self.repos_url)
+        repo_info = self._request_handler(self.repos_url)
         repo_info_list = repo_info.json()
         for repo in repo_info_list:
             if repo.get('name') == self.repo_name:
-                return repo.get('issues_url').replace('{/number}', '')
+                return repo.get('issues_url').replace('{/number}', '?per_page=1000')
 
     def retrive_issue_list(self):
         """
+        get a list of issues
         """
         print("Retriving issue list using api: {0}".format(self.issue_url))
-        repo_info = self.request_handler(self.issue_url)
+        repo_info = self._request_handler(self.issue_url)
         repo_info_list = repo_info.json()
-        print(repo_info_list)
 
+        self._format_issues(repo_info_list)
+
+    def _format_issues(self, repo_info_list):
+        """
+        """
+        data_dict = dict()
+        for cnt, issue in enumerate(repo_info_list):
+            label_list = [label.get('name') for label in issue.get('labels')]
+            data_dict[cnt] = [issue.get('number'),
+                             issue.get('title'),
+                             issue.get('state'),
+                             issue.get('created_at'),
+                             issue.get('updated_at'),
+                             issue.get('closed_at'),
+                             issue.get('assignee'),
+                             label_list,]
+        return data_dict
+
+    def create_csv(self, data_dict):
+        """
+        """
+        
+#HEADER_LIST = ['ID', 'Title', 'State', 'Created At', 'Updated At', 'Closed At', 'Assignee', 'Label']
 if __name__ == "__main__":
 
     # Clear screen
@@ -116,7 +134,7 @@ if __name__ == "__main__":
     password = getpass.getpass()
 
     # Enter org name
-    org_name = input("Enter org name: ")
+    org_name = input("Enter organization name: ")
 
     # Enter repository name
     repo_name = input("Enter repository name: ")
